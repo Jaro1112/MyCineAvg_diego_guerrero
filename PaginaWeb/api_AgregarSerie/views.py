3a741ca0baf98670fa46a8ee55916e8567e25e43
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-05-02 17:51:19 -0500

    Editar series y poderlas ver solo falta lo de eliminar

--- PaginaWeb/api_AgregarSerie/views.py
+++ PaginaWeb/api_AgregarSerie/views.py
@@ -20,3 +20,24 @@ class serie_api_view(APIView):
             serializador.save()
             return Response(serializador.data, status = status.HTTP_200_OK)
         return Response(serializador.data, status = status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk, *args, **kwargs):
+        serie_obj = serie.objects.get(pk=pk)
+        data = {
+            'nombreSerie': request.data.get('nombreSerie', serie_obj.nombreSerie),
+            'duracion': request.data.get('duracion', serie_obj.duracion),
+            'genero': request.data.get('genero', serie_obj.genero),
+            'fechaEstreno': request.data.get('fechaEstreno', serie_obj.fechaEstreno),
+            'pais': request.data.get('pais', serie_obj.pais)
+        }
+        serializador = serie_serializer(serie_obj, data=data, partial=True)
+        if serializador.is_valid():
+            serializador.save()
+            return Response(serializador.data, status=status.HTTP_200_OK)
+        return Response(serializador.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class serie_detail_api_view(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        serie_obj = get_object_or_404(serie, pk=pk)
+        serializador = serie_serializer(serie_obj)
+        return Response(serializador.data, status=status.HTTP_200_OK)
