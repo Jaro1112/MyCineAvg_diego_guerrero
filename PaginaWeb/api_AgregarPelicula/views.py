06318c276b092e2507ee15fa3c4c0373578fbfae
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-05-23 01:49:30 -0500

    cambios menores

--- PaginaWeb/api_AgregarPelicula/views.py
+++ PaginaWeb/api_AgregarPelicula/views.py
@@ -1,28 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from api_AgregarPelicula.models import pelicula
 from api_AgregarPelicula.serializer import pelicula_serializer
-from rest_framework import status, permissions
-import json
 
 class pelicula_api_view(APIView):
     def post(self, request, *args, **kwargs):
         data = {
-            'nombrePelicula' : request.data.get('nombrePelicula'),
-            'duracion' : request.data.get('duracion'),
-            'genero' : request.data.get('genero'),
-            'fechaEstreno' : request.data.get('fechaEstreno'),
-            'pais' : request.data.get('pais')
+            'nombrePelicula': request.data.get('nombrePelicula'),
+            'duracion': request.data.get('duracion'),
+            'genero': request.data.get('genero'),
+            'fechaEstreno': request.data.get('fechaEstreno'),
+            'pais': request.data.get('pais')
         }
-        serializador = pelicula_serializer(data = data)
+        serializador = pelicula_serializer(data=data)
         if serializador.is_valid():
             serializador.save()
-            return Response(serializador.data, status = status.HTTP_200_OK)
-        return Response(serializador.data, status = status.HTTP_400_BAD_REQUEST)
+            return Response(serializador.data, status=status.HTTP_200_OK)
+        return Response(serializador.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def put(self, request, pk, *args, **kwargs):
-        pelicula_obj = pelicula.objects.get(pk=pk)
+        pelicula_obj = get_object_or_404(pelicula, pk=pk)
         data = {
             'nombrePelicula': request.data.get('nombrePelicula', pelicula_obj.nombrePelicula),
             'duracion': request.data.get('duracion', pelicula_obj.duracion),
@@ -36,10 +35,11 @@ class pelicula_api_view(APIView):
             return Response(serializador.data, status=status.HTTP_200_OK)
         return Response(serializador.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def put(selft,request,pk):
-        pelicula_consultada=pelicula.objects.filter(pk=pk).delete()
-        return Response(pelicula_consultada.data,status=status.HTTP_200_OK)
-    
+    def delete(self, request, pk, *args, **kwargs):
+        pelicula_consultada = get_object_or_404(pelicula, pk=pk)
+        pelicula_consultada.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class pelicula_detail_api_view(APIView):
     def get(self, request, pk, *args, **kwargs):
         pelicula_obj = get_object_or_404(pelicula, pk=pk)
