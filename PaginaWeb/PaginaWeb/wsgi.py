6177746498ea4a486763050fac71a12f6914c83c
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-05-23 03:47:22 -0500

    sys enwsgi

--- PaginaWeb/PaginaWeb/wsgi.py
+++ PaginaWeb/PaginaWeb/wsgi.py
@@ -10,5 +10,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
+import sys
+
+# Añadir el directorio del proyecto a sys.path
+sys.path.append('/opt/render/MyCineAvg_diego_guerrero/PaginaWeb')
+sys.path.append('/opt/render/MyCineAvg_diego_guerrero/PaginaWeb/PaginaWeb')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PaginaWeb.settings')
 application = get_wsgi_application()
