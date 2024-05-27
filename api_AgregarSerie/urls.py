2021ab02ec0b04e2af6faf197551bde9c0cf239d
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-05-26 19:34:13 -0500

    reforma en series, mas ver todas las series

--- api_AgregarSerie/urls.py
+++ api_AgregarSerie/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from api_AgregarSerie.views import serie_api_view, serie_detail_api_view
 
-urlpatterns=[
-    path('crear-serie',serie_api_view.as_view()),
-    path('editar-serie/<int:pk>', serie_api_view.as_view()),
-    path('serie/<int:pk>/', serie_detail_api_view.as_view()),
-    path('elliminar-serie/<int:pk>/', serie_api_view.as_view()),
-    
+urlpatterns = [
+    path('crear-serie/', serie_api_view.as_view()),  
+    path('editar-serie/<int:pk>/', serie_api_view.as_view()),  
+    path('serie/<int:pk>/', serie_detail_api_view.as_view()),  
+    path('eliminar-serie/<int:pk>/', serie_api_view.as_view()),  
+    path('series/', serie_api_view.as_view()), #todas las series
 ]
-
-    
