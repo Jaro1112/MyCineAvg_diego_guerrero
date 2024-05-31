d70d0b5a88f5edf9c5e35014fa92207718dc7705
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-05-30 22:29:41 -0500

    nps2

--- api_AgregarPelicula/serializer.py
+++ api_AgregarPelicula/serializer.py
@@ -2,8 +2,6 @@ from api_AgregarPelicula.models import pelicula
 from rest_framework import serializers
 
 class pelicula_serializer(serializers.ModelSerializer):
-    nombrePelicula = serializers.CharField(max_length=100)  
-    duracion = serializers.CharField(max_length=30)  
     class Meta:
         model = pelicula
         fields = ['id', 'nombrePelicula', 'duracion', 'genero', 'fechaEstreno', 'pais', 'imagen_url']
