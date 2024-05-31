223ef8684ff10d457a436b98a921c1fb68584be4
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-05-30 20:02:18 -0500

    migrations

--- PaginaWeb/settings.py
+++ PaginaWeb/settings.py
@@ -137,5 +137,4 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
     "https://mycineavg-diego-guerrero.onrender.com",
-    "green-sea-0032f640f.5.azurestaticapps.net"
 ]
